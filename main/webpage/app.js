5361059c5fb3b76d5cbbac05990fabe412dcd864
lorinczstefanmihai <54044259+lorinczstefanmihai@users.noreply.github.com>
2025-10-29 12:36:43 +0200

    local webpage update

--- main/webpage/app.js
+++ main/webpage/app.js
@@ -3,12 +3,22 @@
  */
 var seconds 	= null;
 var otaTimerVar =  null;
+var wifiConnectInterval = null;
 
 /**
  * Initialize functions here.
  */
 $(document).ready(function(){
 	getUpdateStatus();
+    startDHTSensorInterval();
+    startMQSensorInterval(); 
+	getConnectInfo();
+    $("#connect_wifi").on("click",function(){
+		checkCredentials();   
+    });
+	$("#disconnect_wifi").on("click", function(){
+		disconnectWifi();
+	}); 
 });   
 
 /**
@@ -114,6 +124,204 @@ function otaRebootTimer()
 	{
         otaTimerVar = setTimeout(otaRebootTimer, 1000);
     }
+}
+
+/**
+ * Gets DHT11 sensor temperature and humidity values for display on the webpage
+ */
+function getDHTSensorValues()
+{
+    $.getJSON('/dhtSensor.json', function(data){
+        $('#temperature_reading').text(data["temp"]);
+        $('#humidity_reading').text(data["humidity"]);
+    });
+}
+
+/**
+ * Sets interval to get DHT11 sensor values every 1 second
+ */
+function startDHTSensorInterval()
+{
+    setInterval(getDHTSensorValues, 1000);
+}
+
+/**
+ * Gets DHT11 sensor temperature and humidity values for display on the webpage
+ */
+function getMQSensorValues()
+{
+    $.getJSON('/mqSensor.json', function(data){
+        $('#raw_reading').text(data["raw"]);
+        $('#volt_reading').text(data["volt"]);
+    });
+}
+
+/**
+ * Sets interval to get MQ135 sensor values every 1 second
+ */
+function startMQSensorInterval()
+{
+    setInterval(getMQSensorValues, 1000);
+}
+
+/**
+ * Clear the connection status interval
+ */
+function stopWifiConnectStatusInterval()
+{
+	if (wifiConnectInterval != null)
+	{
+		clearInterval(wifiConnectInterval);
+		wifiConnectInterval = null;
+	}
+}
+
+/**
+ * Gets the WiFi connection status
+ */
+function getWifiConnectStatus()
+{
+	var xhr = new XMLHttpRequest();
+	var requestURL = "/wifiConnectStatus";
+	xhr.open('POST', requestURL, false);
+	xhr.send('wifi_connect_status');
+	
+	if (xhr.readyState == 4 && xhr.status == 200)
+	{
+		var response = JSON.parse(xhr.responseText);
+		
+		document.getElementById("wifi_connect_status").innerHTML = "Connecting...";
+		
+		if (response.wifi_connect_status == 2)
+		{
+			document.getElementById("wifi_connect_status").innerHTML = "<h4 class='rd'>Failed to Connect. Please check your AP credentials and compatibility</h4>";
+			stopWifiConnectStatusInterval();
+		}
+		else if (response.wifi_connect_status == 3)
+		{
+			document.getElementById("wifi_connect_status").innerHTML = "<h4 class='gr'>Connection Success!</h4>";
+			stopWifiConnectStatusInterval();
+			getConnectInfo();
+		}
+	}
+}
+
+/**
+ * Starts the interval for checking the connection status
+ */
+function startWifiConnectStatusInterval()
+{
+    wifiConnectInterval = setInterval(getWifiConnectStatus, 2800);
+}
+
+
+/**
+ * Connect WiFi function called using the SSID and password entered into the text fields.
+ */
+function connectWifi()
+{
+	// Get the SSID and password
+	selectedSSID = $("#connect_ssid").val();
+	pwd = $("#connect_pass").val();
+	
+	$.ajax({
+		url: '/wifiConnect.json',
+		dataType: 'json',
+		method: 'POST',
+		cache: false,
+		headers: {'my-connect-ssid': selectedSSID, 'my-connect-pwd': pwd},
+		data: {'timestamp': Date.now()}
+	});
+	
+	startWifiConnectStatusInterval();
+}
+
+/**
+ * Checks credentials on connect_wifi button click.
+ */
+function checkCredentials()
+{
+	errorList = "";
+	credsOk = true;
+	
+	selectedSSID = $("#connect_ssid").val();
+	pwd = $("#connect_pass").val();
+	
+	if (selectedSSID == "")
+	{
+		errorList += "<h4 class='rd'>SSID cannot be empty!</h4>";
+		credsOk = false;
+	}
+	if (pwd == "")
+	{
+		errorList += "<h4 class='rd'>Password cannot be empty!</h4>";
+		credsOk = false;
+	}
+	
+	if (credsOk == false)
+	{
+		$("#wifi_connect_credentials_errors").html(errorList);
+	}
+	else
+	{
+		$("#wifi_connect_credentials_errors").html("");
+		connectWifi();    
+	}
+}
+
+/**
+ * Shows the WiFi password if the box is checked.
+ */
+function showPassword()
+{
+	var x = document.getElementById("connect_pass");
+	if (x.type === "password")
+	{
+		x.type = "text";
+	}
+	else
+	{
+		x.type = "password";
+	}
+}
+
+/**
+ * Gets the connection information for displaying on the web page.
+ */
+function getConnectInfo()
+{
+	$.getJSON('/wifiConnectInfo.json', function(data)
+	{
+		$("#connected_ap_label").html("Connected to: ");
+		$("#connected_ap").text(data["ap"]);
+		
+		$("#ip_address_label").html("IP Address: ");
+		$("#wifi_connect_ip").text(data["ip"]);
+		
+		$("#netmask_label").html("Netmask: ");
+		$("#wifi_connect_netmask").text(data["netmask"]);
+		
+		$("#gateway_label").html("Gateway: ");
+		$("#wifi_connect_gw").text(data["gw"]);
+		
+		document.getElementById('disconnect_wifi').style.display = 'block';
+	});
+}
+
+/**
+ * Disconnects WiFi once the disconnect button is pressed and reloads the page.
+ */
+function disconnectWifi()
+{
+	$.ajax({
+		url: '/wifiDisconnect.json',
+		dataType: 'json',
+		method: 'DELETE',
+		cache: false,
+		data: { 'timestamp': Date.now() }
+	});
+	// Update the web page
+	setTimeout("location.reload(true);", 2000);
 }
 
 
